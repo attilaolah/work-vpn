936c68fcf96a3d0fe3d4e6ffa0c3d57844e1934b
Attila Oláh <attila@dorn.haus>
2026-01-09 16:08:25 +0100

    refactor: convert shell script to shell application

--- flake.nix
+++ flake.nix
@@ -10,26 +10,20 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
       perSystem = {pkgs, ...}: {
-        packages.default = with pkgs.lib; let
-          ask-password = getExe' pkgs.systemd "systemd-tty-ask-password-agent";
-          bc = getExe pkgs.bc;
-          cat = getExe' pkgs.coreutils "cat";
-          echo = getExe' pkgs.coreutils "echo";
-          grep = getExe pkgs.gnugrep;
-          mkfifo = getExe' pkgs.coreutils "mkfifo";
-          mktemp = getExe' pkgs.coreutils "mktemp";
-          openvpn = getExe pkgs.openvpn;
-          rbw = getExe pkgs.rbw;
-          reply-password = "${pkgs.systemd}/lib/systemd/systemd-reply-password";
-          rm = getExe' pkgs.coreutils "rm";
-          rmdir = getExe' pkgs.coreutils "rmdir";
-          sed = getExe pkgs.gnused;
-          sleep = getExe' pkgs.coreutils "sleep";
-          tr = getExe' pkgs.coreutils "tr";
-          update-resolv-conf = "${pkgs.update-resolv-conf}/libexec/openvpn/update-resolv-conf";
-          wc = getExe' pkgs.coreutils "wc";
-        in
-          pkgs.writeShellScriptBin "work-vpn" ''
+        packages.default = pkgs.writeShellApplication {
+          name = "work-vpn";
+          runtimeInputs = with pkgs; [
+            bc
+            coreutils
+            gnugrep
+            gnused
+            openvpn
+            rbw
+            sudo
+            systemd
+            update-resolv-conf
+          ];
+          text = ''
             set -euo pipefail
 
             while getopts ":vs-:" opt; do
@@ -68,21 +62,21 @@
             # Bitwarden credentials identifier.
             # This is where the VPN username & password are stored.
             if [ -z "''\${OPENVPN_BW_ID:-}" ]; then
-              ${echo} "OPENVPN_BW_ID environment variable is not set."
-              ${echo} "Store work credentials in Bitwarden and set the UUID in \`.env.local\`."
+              echo "OPENVPN_BW_ID environment variable is not set."
+              echo "Store work credentials in Bitwarden and set the UUID in \`.env.local\`."
               exit 2
             fi
 
             # Ensure RBW is logged in.
-            if ! ${rbw} login; then
-              ${echo} "Bitwarden login failed. Make sure \`rbw\` is installed and the \`rbw-agent\`"
-              ${echo} "is running. Install \`rbw\` and type \`rbw login\` to get started."
+            if ! rbw login; then
+              echo "Bitwarden login failed. Make sure \`rbw\` is installed and the \`rbw-agent\`"
+              echo "is running. Install \`rbw\` and type \`rbw login\` to get started."
               exit 3
             fi
 
             # Ensure RBW is unlocked.
-            if ! ${rbw} unlock; then
-              ${echo} "Bitwarden unlock failed. Try unlocking manually by running \`rbw unlock\`."
+            if ! rbw unlock; then
+              echo "Bitwarden unlock failed. Try unlocking manually by running \`rbw unlock\`."
               exit 4
             fi
 
@@ -96,17 +90,17 @@
               VERB=0
             fi
 
-            CREDS_DIR="$(${mktemp} --directory)"
+            CREDS_DIR="$(mktemp --directory)"
             CREDS_FIFO="$CREDS_DIR/credentials"
-            ${mkfifo} --mode=600 "$CREDS_FIFO"
+            mkfifo --mode=600 "$CREDS_FIFO"
 
-            cat <<EOF >$CREDS_FIFO &
-            $(${rbw} get $OPENVPN_BW_ID --field username)
-            $(${rbw} get $OPENVPN_BW_ID --field password)
+            cat <<EOF >"$CREDS_FIFO" &
+            $(rbw get "$OPENVPN_BW_ID" --field username)
+            $(rbw get "$OPENVPN_BW_ID" --field password)
             EOF
             CREDS_PID=$!
 
-            ${cat} <<EOF |
+            cat <<EOF |
             client
             nobind
 
@@ -137,8 +131,8 @@
 
             # Update resolv.conf when connected.
             # Needed to get internal domains to resolve.
-            up ${update-resolv-conf}
-            down ${update-resolv-conf}
+            up "${pkgs.update-resolv-conf}/libexec/openvpn/update-resolv-conf"
+            down "${pkgs.update-resolv-conf}/libexec/openvpn/update-resolv-conf"
             script-security 2
 
             # Access Server:
@@ -149,8 +143,8 @@
             <ca>
             -----BEGIN CERTIFICATE-----
             $(
-              ${rbw} get $OPENVPN_BW_ID --field openvpn_ca |
-                ${tr} ' ' \\n
+              rbw get "$OPENVPN_BW_ID" --field openvpn_ca |
+                tr ' ' \\n
             )
             -----END CERTIFICATE-----
             </ca>
@@ -160,50 +154,50 @@
             <tls-crypt-v2>
             -----BEGIN OpenVPN tls-crypt-v2 client key-----
             $(
-              ${rbw} get $OPENVPN_BW_ID --field openvpn_tls_client_key |
-                ${tr} ' ' \\n
+              rbw get "$OPENVPN_BW_ID" --field openvpn_tls_client_key |
+                tr ' ' \\n
             )
             -----END OpenVPN tls-crypt-v2 client key-----
             </tls-crypt-v2>
             EOF
-              sudo ${openvpn} --config /dev/stdin &
+              sudo openvpn --config /dev/stdin &
             START_PID=$!
 
             # Clean up temporary files.
             wait $CREDS_PID
-            ${rm} "$CREDS_FIFO"
-            ${rmdir} "$CREDS_DIR"
+            rm "$CREDS_FIFO"
+            rmdir "$CREDS_DIR"
 
             # Wait for 2nd factor prompt:
             CHALLENGE_PAT="CHALLENGE: "
             while [ "$(
-              ${ask-password} --list |
-                ${grep} "$CHALLENGE_PAT" |
-                ${wc} -l
+              systemd-tty-ask-password-agent --list |
+                grep -c "$CHALLENGE_PAT"
             )" -lt 1 ]; do
-              ${sleep} 0.2
+              sleep 0.2
             done
 
             RESPONSE="$OPENVPN_CHALLENGE_PREFIX$(
-              ${ask-password} --list |
-                ${grep} "$CHALLENGE_PAT" |
-                ${sed} --regexp-extended --expression 's/.*?([0-9]+)([x/+-])([0-9]+).*/\1\2\3/g' |
-                ${tr} x '*' |
-                ${bc}
+              systemd-tty-ask-password-agent --list |
+                grep "$CHALLENGE_PAT" |
+                sed --regexp-extended --expression 's/.*?([0-9]+)([x/+-])([0-9]+).*/\1\2\3/g' |
+                tr x '*' |
+                bc
             )"
             SOCKET="$(
-              ${grep} "^Socket=" $(
-                ${grep} --files-with-matches "$CHALLENGE_PAT" /run/systemd/ask-password/ask.*
-              ) |
-              ${sed} 's/.*=//'
+              grep "^Socket=" "$(
+                grep --files-with-matches "$CHALLENGE_PAT" /run/systemd/ask-password/ask.*
+              )" |
+              sed 's/.*=//'
             )"
 
-            ${echo} "$RESPONSE" |
-              sudo pkexec "${reply-password}" 1 "$SOCKET"
+            echo "$RESPONSE" |
+              sudo pkexec "${pkgs.systemd}/lib/systemd/systemd-reply-password" 1 "$SOCKET"
 
             # Give control back to the OpenVPN process:
             wait $START_PID
           '';
+        };
       };
     };
 }
